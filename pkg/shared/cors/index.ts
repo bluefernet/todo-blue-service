85f05dc22430d800b48b1f801978c78b4db4f3db
Enrique Taietta <e.taietta@happenin.it>
2020-04-29 21:47:36 +0200

    Inserted cors options in http requests

---
+++ pkg/shared/cors/index.ts
@@ -0,0 +1,18 @@
+const corsOptions = {
+	origin: [
+		"https://todo-blue-ui-staging.now.sh"
+	],
+	methods: [
+		"GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"
+	],
+	allowedHeaders: [
+		"Content-Type",
+		"Access-Control-Allow-Headers",
+		"Authorization",
+		"X-Requested-With"
+	],
+	preflightContinue: false,
+	optionsSuccessStatus: 200
+};
+
+export default corsOptions;
--- pkg/tasks/transport/http/tasks-endpoint.ts
+++ pkg/tasks/transport/http/tasks-endpoint.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from 'express';
 import express from 'express';
 import { json } from 'body-parser';
+import { urlencoded } from "body-parser";
 import {
 	asyncTasksList,
 	asyncCreateTask
 } from '../../service/base/tasks-service';
 import { Task } from '../../../shared/types'
+import corsOptions from "../../../shared/cors";
 
 const app = express();
+const cors = require("cors");
+
 app.use(json());
+app.use(json());
+app.use(urlencoded());
+app.use(cors(corsOptions));
 
 // POST - /tasks
 
 app.get(
 	'*',
+	cors(corsOptions),
 	async (req: Request, res: Response): Promise<void> => {
 		let tasksList = await asyncTasksList();
 		res.json({
@@ -26,7 +34,7 @@ app.get(
 
 app.post(
 	'*',
-	// jwt('secret'),
+	cors(corsOptions),
 	async (req: Request, res: Response): Promise<void> => {
 		const task: Task = req.body;
 		let data: Task = await asyncCreateTask(task);
